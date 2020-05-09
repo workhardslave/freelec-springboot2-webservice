01589cc057380d6648d4e7a46cdcbdaa13d9eec2
workhardslave <mae0428@naver.com>
2020-05-10 01:21:19 +0900

    5단원 진행중

--- src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -11,7 +11,7 @@
  * 조회한 Entity의 값을 변경할 때 시간이 자동 저장된다.
  */
 
-package com.jojoldu.book.springboot.domain.posts;
+package com.jojoldu.book.springboot.domain;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -28,6 +28,7 @@
 
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,7 +42,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts extends BaseTimeEntity{
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
