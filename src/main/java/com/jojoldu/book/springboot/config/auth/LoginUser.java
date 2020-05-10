ac0f9696b89e39bb0b450ccda2c287f98386ef52
workhardslave <mae0428@naver.com>
2020-05-10 19:19:24 +0900

    5단원 진행중 : 네이버 로그인 완료

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,21 @@
+/* 1. @Target(ElementType.PARAMETER)
+ * 이 어노테이션이 생성될 수 있는 위치를 지정
+ * PARAMETER로 지정했으니 메소드의 파라미터로 선언된 객체에서만 사용할 수 있음
+ * 이 외에도 클래스 선언문에 쓸 수 있는 TYPE 등이 있음
+ *
+ * 2. @Interface
+ * 이 파일을 어노테이션 클래스로 지정
+ * LoginUser 라는 이름을 가진 어노테이션이 생성
+ */
+
+package com.jojoldu.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,18 @@
+/* 1. (SessionUser) httpSession.getAttribute("user")
+ * 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+ * 로그인 성공 시 httpSession.getAttributes("user")에서 값을 가져올 수 있음
+ * 
+ * 2. if(user != null)
+ * 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+ * 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+ * 
+ * 3. @LoginUser SessionUser user
+ * 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
+ * 이제는 어느 컨트롤러든지 @LoginUser 만 사용하면 세션 정보를 가져올 수 있음
+ */
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +32,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName",user.getName());
         }
