1b1a6dccd97d6a549ed63c875d8e8b41c1d7a981
workhardslave <mae0428@naver.com>
2020-05-11 16:55:51 +0900

    5단원 완료
    스프링 시큐리티 + OAuth2.0
    로그인 기능 구현

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -8,20 +8,21 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_로딩(){
-        // when
-        String body = this.restTemplate.getForObject("/",String.class);
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
 
-        // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
